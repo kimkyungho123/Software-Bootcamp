8500706efc0c4031fa7edd3d4a9f2ed419e04b7d
Kyung Ho Kim <kck18949@naver.com>
2023-02-06 23:20:11 +0900

    File Input&Output

--- Practice_Sheet.py
+++ Practice_Sheet.py
@@ -1,26 +1,41 @@
-#Various Input Format
+# File Input&Output
 
-#빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
-print("{0: >10}".format(500))
+# score_file = open("score.txt", "w", encoding="utf8")
+# print("수학 : 0", file=score_file)
+# print("영어 : 50", file=score_file)
+# score_file.close()
 
-#양수일 때는 +표시, 음수일 때는 -표시
-print("{0: >+10}".format(500))
-print("{0: >+10}".format(-500))
 
-#왼쪽 정렬하고, 빈칸을 _로 채움
-print("{0:_<+10}".format(500))
+# score_file = open("score.txt", "a", encoding="utf8")
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
 
-#세자리 마다 콤마를 찍기
-print("{0:,}".format(1000000000))
 
-#세자리 마다 콤마를 찍기, +- 부호도 붙이기
-print("{0:+,}".format(1000000000))
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.read())
+# score_file.close()
 
-#세자리 마다 콤마를 찍기, +- 부호도 붙이기, 자릿수 확보하기, 빈 자리는 ^로 채우기
-print("{0:^<+30,}".format(1000000000))
 
-#소수점 출력
-print("{0:f}".format(5/3))
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline()) #한 줄 읽고 커서는 다음 줄로 이동
+# print(score_file.readline()) #줄바꿈이 싫으면 end="" 사용
+# print(score_file.readline())
+# print(score_file.readline())
+# score_file.close()
 
-#소수점 특정 자릿수 까지만 표시
-print("{0:.2f}".format(5/3))
+
+#가져온 파일이 몇 줄인지 모를 때
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line: #읽어올 내용이 없으면
+#         break
+#     print(line)
+# score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+lines = score_file.readlines() #모든 line을 list 형태로 저장함
+for line in lines:
+    print(line, end="")
+score_file.close()
