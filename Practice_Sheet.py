d2b975276a615cc5c83870db71b59be871a70966
Kyung Ho Kim <kck18949@naver.com>
2023-02-06 20:37:53 +0900

    function2

--- Practice_Sheet.py
+++ Practice_Sheet.py
@@ -1,16 +1,32 @@
 # def profile(name, age, main_lang):
-#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
+#     print(name, age, main_lang)
 #
 #
-# profile("유재석", 20, "파이썬")
-# profile("김태호", 25, "자바")
+# profile(name="유재석", main_lang="파이썬", age=20,)
+# profile(main_lang="자바", name="김태호", age=20)
 
 
 
-#기본값
-def profile(name, age=17, main_lang="파이썬"):
-     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
 
 
-profile("유재석")
-profile("김태호")
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+#     print(lang1, lang2, lang3, lang4, lang5)
+#
+#
+# profile("유재석", 20, "python", "Java", "C", "C++", "C#")
+# profile("김태호", 25, "Kotlin", "Swift", "", "", "")
+
+
+
+
+#가변인자
+def profile(name, age, *language):
+    print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+    for lang in language:
+        print(lang, end=" ")
+    print()
+
+
+profile("유재석", 20, "python", "Java", "C", "C++", "C#", "JavaScript")
+profile("김태호", 25, "Kotlin", "Swift")
