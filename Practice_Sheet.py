ce4dc2624b3ba91c80744dbd36bc21d4b1ff8d0f
Kyung Ho Kim <kck18949@naver.com>
2023-02-06 22:54:25 +0900

    Various Input Format

--- Practice_Sheet.py
+++ Practice_Sheet.py
@@ -1,27 +1,26 @@
-# print("Python", "Java", sep=", ", end="?")
-# print("무엇이 더 재밌을까요?")
+#Various Input Format
 
+#빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+print("{0: >10}".format(500))
 
+#양수일 때는 +표시, 음수일 때는 -표시
+print("{0: >+10}".format(500))
+print("{0: >+10}".format(-500))
 
-# import sys
-# print("Python", "Java", file=sys.stdout) #표준출력
-# print("Python", "Java", file=sys.stderr) #표준에러
+#왼쪽 정렬하고, 빈칸을 _로 채움
+print("{0:_<+10}".format(500))
 
+#세자리 마다 콤마를 찍기
+print("{0:,}".format(1000000000))
 
+#세자리 마다 콤마를 찍기, +- 부호도 붙이기
+print("{0:+,}".format(1000000000))
 
-# scores = {"수학": 0, "영어": 50, "코딩": 100}
-# for subject, score in scores.items():
-#     # print(subject, score)
-#     print(subject.ljust(3), str(score).rjust(4), sep=":")
+#세자리 마다 콤마를 찍기, +- 부호도 붙이기, 자릿수 확보하기, 빈 자리는 ^로 채우기
+print("{0:^<+30,}".format(1000000000))
 
+#소수점 출력
+print("{0:f}".format(5/3))
 
-# 은행 대기순번표 ex) 001, 002, 003, ...
-# for num in range(1,21):
-#     print("대기번호 : " + str(num).zfill(3))
-
-
-
-answer = input("아무 값이나 입력하세요 : ")
-# 중요: input()함수는 항상 문자열로 값을 저장한다
-print(type(answer))
-print("입력하신 값은 " + answer + " 입니다.")
+#소수점 특정 자릿수 까지만 표시
+print("{0:.2f}".format(5/3))
