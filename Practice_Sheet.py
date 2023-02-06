185ad6507f4fcaf2b8c904aedf416f4c078a3b14
Kyung Ho Kim <kck18949@naver.com>
2023-02-06 20:11:16 +0900

    function

--- Practice_Sheet.py
+++ Practice_Sheet.py
@@ -1,28 +1,16 @@
-def open_account():
-    print("새로운 계좌가 생성되었습니다.")
+# def profile(name, age, main_lang):
+#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
+#
+#
+# profile("유재석", 20, "파이썬")
+# profile("김태호", 25, "자바")
 
 
-def deposit(balance, money):
-    print("입금이 완료되었습니다. 잔액은 {}원 입니다.".format(balance + money))
-    return balance + money
+
+#기본값
+def profile(name, age=17, main_lang="파이썬"):
+     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
 
 
-def withdraw(balance, money):
-    if balance >= money:
-        print("출금이 완료되었습니다. 잔액은 {0}원 입니다.".format(balance - money))
-        return balance - money
-    else:
-        print("출금이 완료되지 않았습니다. 잔액은 {}원 입니다.". format(balance))
-        return balance
-
-
-def withdraw_night(balance, money):
-    commission = 100
-    return commission, balance - money - commission
-
-
-balance = 0
-balance = deposit(balance, 50000)
-balance = withdraw(balance, 40000)
-commission, balance = withdraw_night(balance, 500)
-print("수수료는 {}원이며, 잔액은 {}원 입니다.".format(commission, balance))
+profile("유재석")
+profile("김태호")
