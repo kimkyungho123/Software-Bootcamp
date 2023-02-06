26cf6f14c75cfa6523f31223580824fcf4637e8d
Kyung Ho Kim <kck18949@naver.com>
2023-02-06 21:13:38 +0900

    Standard Weight Example

--- Practice_Sheet.py
+++ Practice_Sheet.py
@@ -1,21 +1,13 @@
-#전역변수&지역변수
+# Standard Weight Example
 
-gun = 10
-
-
-def checkpoint(soldiers):
-    global gun
-    gun = gun - soldiers
-    print("[함수 내] 남은 총 : {0}".format(gun))
+def std_weight(height, gender):
+    if gender == "남자":
+        return height * height * 22
+    else:
+        return height * height * 21
 
 
-def checkpoint_return(gun, soldiers):
-    gun = gun - soldiers
-    print("[함수 내] 남은 총 : {0}".format(gun))
-    return gun
-
-
-print("전체 총 : {0}".format(gun))
-# checkpoint(2)
-gun = checkpoint_return(gun, 2)
-print("남은 총 : {0}".format(gun))
+height = 175
+gender = "남자"
+weight = round(std_weight(height / 100, gender), 2)
+print("키 {0}cm인 {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
