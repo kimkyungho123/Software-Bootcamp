ece4bc6387385232f3e31c035b5bbfdbd2e5d897
Kyung Ho Kim <kck18949@naver.com>
2023-02-07 18:26:26 +0900

    Method Overriding & Pass

--- Practice_Sheet.py
+++ Practice_Sheet.py
@@ -1,15 +1,18 @@
-# multiple inheritance
-# self: 객체 자기 자신
-
+# Method Overriding
+# Pass
 class unit:
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
 
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name, location, self.speed))
 
 class attack_unit(unit):
-    def __init__(self, name, hp, damage):
-        unit.__init__(self, name, hp)
+    def __init__(self, name, hp, speed, damage):
+        unit.__init__(self, name, hp, speed)
         self.damage = damage
 
     def attack(self, location):
@@ -33,9 +36,40 @@ class flyable:
 
 class flyable_attack_unit(attack_unit, flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        attack_unit.__init__(self, name, hp, damage)
+        attack_unit.__init__(self, name, hp, 0, damage) # 지상 speed = 0
         flyable.__init__(self, flying_speed)
 
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
 
-valkyrie = flyable_attack_unit("발키리", 200, 6, 5)
-valkyrie.fly(valkyrie.name, "3시")
+
+vulture = attack_unit("벌쳐", 80, 10, 20)
+
+BatttleCruiser = flyable_attack_unit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+BatttleCruiser.fly(BatttleCruiser.name, "9시")
+
+BatttleCruiser.move("9시")
+
+#-----------------------------------------------------
+
+class building_unit(unit):
+    def __init__(self, name, hp, location):
+        pass
+
+
+supply_depot = building_unit("서플라이 디폿", 500, "7시")
+
+
+def game_start_():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+
+def game_over():
+    pass
+
+
+game_start_()
+game_over()
