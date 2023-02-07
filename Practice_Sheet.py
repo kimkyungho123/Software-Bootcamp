53be57cbb32118e667312bffc94339b91a745832
Kyung Ho Kim <kck18949@naver.com>
2023-02-07 23:00:26 +0900

    Chicken Ordering

--- Practice_Sheet.py
+++ Practice_Sheet.py
@@ -1,23 +1,28 @@
-# Finally
+#  Chicken Ordering
 
-class big_number_error(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+class sold_out_error(Exception):
+    pass
 
-    def __str__(self):
-        return self.msg
+chicken = 10
+waiting = 1
 
+while(True):
+    try:
+        print("[남은 치킨 : {0}]".format(chicken))
+        order = int(input("치킨을 몇 마리 주문하시겠습니까?"))
+        if order > chicken:
+            print("재료가 부족합니다.")
+        elif order <= 0:
+            raise ValueError
+        else:
+            print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다.".format(waiting, order))
+            waiting += 1
+            chicken -= order
 
-try:
-    print("<한 자리 숫자 나누기 전용 계산기입니다.>")
-    num1 = int(input("첫 번재 숫자를 입력하세요 : "))
-    num2 = int(input("두 번재 숫자를 입력하세요 : "))
-    if num1 >= 10 or num2 >=10:
-        raise big_number_error("입력값 : {0}, {1}".format(num1, num2))
-    print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
-except big_number_error as err:
-    print("에러가 발생했습니다. 한 자리 숫자만 입력하세요.")
-    print(err)
-
-finally:
-    print("계산기를 이용해 주셔서 감사합니다.")
+        if chicken == 0:
+            raise sold_out_error
+    except ValueError:
+        print("잘못된 값을 입력하였습니다.")
+    except sold_out_error:
+        print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+        break
