7d4f6ba3532e314406c982cee5560e3f7e595f6a
Kyung Ho Kim <kck18949@naver.com>
2023-02-08 13:50:20 +0900

    deque

--- Practice_Sheet.py
+++
@@ -1,6 +0,0 @@
-# __all__ & __name__ == "__main__"
-
-from travel import *
-# trip_to = vietnam.vietnam_package()
-trip_to = thailand.thailand_package()
-trip_to.detail()
---
+++ day18.py
@@ -0,0 +1,55 @@
+## 클래스 및 함수 선언 부분 ##
+
+from collections import deque
+
+class Graph() :
+	def __init__ (self, size) :
+		self.SIZE = size
+		self.graph = [[0 for _ in range(size)] for _ in range(size)]
+
+## 전역 변수 선언 부분 ##
+G1 = None
+# queue = [] #append() as push() pop() and built in pop()
+queue = deque([])
+visited_array = []		# 방문한 정점
+
+## 메인 코드 부분 ##
+G1 = Graph(4)
+G1.graph[0][2] = 1; G1.graph[0][3] = 1
+G1.graph[1][2] = 1
+G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
+G1.graph[3][2] = 1; G1.graph[3][1] = 1
+
+
+print('## G1 무방향 그래프 ##')
+for row in range(4) :
+	for col in range(4) :
+		print(G1.graph[row][col], end = ' ')
+	print()
+
+current = 0		# 시작 정점 A
+queue.append(current)
+visited_array.append(current)
+
+while len(queue) != 0:
+	next = None
+	for vertex in range(4) :
+		if G1.graph[current][vertex] == 1:
+			if vertex in visited_array :  	   # 방문한 적이 있는 정점이면 탈락
+				pass
+			else : 			   # 방문한 적이 없으면 다음 정점으로 지정
+				next = vertex
+				break
+
+	if next != None :			  	   # 다음에 방문할 정점이 있는 경우
+		current = next
+		queue.append(current)
+		visited_array.append(current)
+	else :            	  	  	  	  # 다음에 방문할 정점이 없는 경우
+		current = queue.popleft()
+
+
+print('방문 순서 -->', end='')
+for i in visited_array :
+	print(i, end =' --> ')
+print("END")
